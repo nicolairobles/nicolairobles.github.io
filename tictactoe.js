b5a2a77c683b7883052e605480a5cc73bc4a06af
Nicolai Robles <nicolairobles>
2015-12-18 10:20:53 -0500

    save project 1 on Friday

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // $(document).ready(function () {
-	// debugger;
 // Set up variables
 	/* Tic Tac Toe Board Structure
 		  1 a  |  1 b |  1 c  
@@ -29,13 +28,8 @@
 	player = 1; // Game starts with Player X
 
 // HUMAN TO HUMAN GAME
-	var switchPlayer; //switch from one player to another
-	var makeMark; //place current player's symbol in box
-	var checkLocation; //check which box was selected
-	var updateGameBoard; //update track of all marks
-	var checkIfWinner; //check if there's now a winner
 // Switch Players Function
-	switchPlayer = function(){
+	var switchPlayer = function(){
 		console.log("switched player");
 		if(player === 1){
 			player = 0; 
@@ -44,60 +38,225 @@
 		}
 	};
 // Make mark in specified div
-	makeMark = function(){
+	var makeMark = function(location){
 		console.log("made mark")
 		if (player === 1){
-			$(this).text("X");
+			$(location).text("X");
 		} else  {
-			$(this).text("O");
+			$(location).text("O");
 		}	
 	}
 // Check Location Function
-	var currentRowIndex;
-	var currentColIndex;
-	checkLocation = function(){
+	var checkLocation = function(location){
+		var currentRowIndex;
+		var currentColIndex;
 		// Get current row and store it
-		if ($(this).hasClass("one")){
+		if ($(location).hasClass("one")){
 			currentRowIndex = 0;
-			console.log("row 1")
-		} else if ($(this).hasClass("two")){
+		} else if ($(location).hasClass("two")){
 			currentRowIndex = 1;
-		} else if ($(this).hasClass("three")){
+		} else if ($(location).hasClass("three")){
 			currentRowIndex = 2;
 		} 
 		// Get current column and store it
-		if ($(this).hasClass("a")){
+		if ($(location).hasClass("a")){
 			currentColIndex = 0;
-			console.log("col a")
-		} else if ($(this).hasClass("b")){
+		} else if ($(location).hasClass("b")){
 			currentColIndex = 1;
-		} else if ($(this).hasClass("c")){
+		} else if ($(location).hasClass("c")){
 			currentColIndex = 2;		
 		}
 		// Update game board with correct symbol
-		gameBoard[currentRowIndex][currentColIndex] = $(this).text();
+		gameBoard[currentRowIndex][currentColIndex] = $(location).text();
+		console.log("location noted in gameboard array")
 	}
-
 // Check if there's a winner function
-	var checkIfVerticalWinner = function(){
-			gameBoard.forEach(function(){
-				
-			})
+	var horizontalPts;
+	var verticalPts;
+	var diagonalPts;
+
+	var checkIfHorizontalWinner = function(){
+		for (var i = 0; i < 3; i++ ){
+			if(gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2] && gameBoard[i][2] != null){
+				console.log("winner in row " + (i+1));
+			}
 		}
-	var checkIfHorizontalWinner;
-	var checkIfDiagonalWinner;
-	checkIfWinner = function(){
-		
+	}
+	var checkIfVerticalWinner = function(){
+		for (var i = 0; i < 3; i++ ){
+			if(gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i] && gameBoard[1][i] != null){
+				console.log("winner in column " + (i+1));
+			}
+		}
+	}
+	var checkIfDiagonalWinner = function(){
+		if (gameBoard[2][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[0][2] && gameBoard[0][2] != null){
+				console.log("winner diagonally");			
+		} else if( gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2] && gameBoard[2][2] != null){
+				console.log("winner diagonally");			
+		}
 	}
 
 
+//Diagonal Almost
+	// 	if (gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] != null){
+	// 		console.log("almost diagonal winner")
+	// 		diagonalPts = 2;
+	// 		if(gameBoard[1][1] === gameBoard[2][2]){
+	// 			diagonalPts = 3;
+	// 			console.log("winner diagonally")
+	// 		}
+	// 	} else if(gameBoard[2][0] === gameBoard[1][1] && gameBoard[1][1] != null){
+	// 		diagonalPts = 2;
+	// 		if(gameBoard[1][1] === gameBoard[2][2]){
+	// 			diagonalPts = 3;
+	// 			console.log("winner diagonally")
+	// 		}
+	// 	}
+	// }
 
-// Event listeners to switch players and paint X or O
-	box.on("click", makeMark);
-	box.on("click", switchPlayer);
-	box.on("click", checkLocation);
+//Horizontal Almost
+	// 	if(gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][1] != null){
+	// 		horizontalPts = 2;
+	// 		if(gameBoard[0][1] === gameBoard[0][2]){
+	// 			horizontalPts = 3;
+	// 			console.log("winner in row 1")
+	// 		}
+	// 	} else if(gameBoard[1][0] === gameBoard[1][1] && gameBoard[1][1] != null){
+	// 		horizontalPts = 2;
+	// 		if(gameBoard[1][1] === gameBoard[1][2]){
+	// 			horizontalPts = 3;
+	// 			console.log("winner in row 2")
+	// 		}
+	// 	} else if (gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][1] != null){
+	// 		horizontalPts = 2;
+	// 		if(gameBoard[2][1] === gameBoard[2][2]){
+	// 			horizontalPts = 3;
+	// 			console.log("winner in row 3")
+	// 		}
+	// 	}
+	// }
+
+	var checkIfAlmostVerticalWinner = function(){
+		for (var i = 0; i < 3; i++ ){
+			//Check first two first
+			if(gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] != null){
+				var blockPosition = box3A;
+				makeMark(blockPosition);
+				console.log("block position: [2]["+i+"]")
+				//Check corners first
+			} else if (gameBoard[0][i] === gameBoard[2][i] && gameBoard[2][i] != null){
+				var blockPosition = box2A;
+				makeMark(blockPosition);
+				console.log("block position: [1]["+i+"]")
+				//Check last two first
+			} else if (gameBoard[1][i] === gameBoard[2][i] && gameBoard[2][i] != null){
+				var blockPosition = box1A;
+				makeMark(blockPosition);
+				console.log("block position: [0]["+i+"]")
+			}
+		}
+	}
+
+	var checkIfAlmostHorizontalWinner = function(){
+		for (var i = 0; i < 3; i++ ){
+			//Check first two first
+			if(gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] != null){
+				var blockPosition = $("."+i);
+				makeMark(blockPosition);
+				console.log("block position:["+i+"][2]")
+				//Check corners first
+			} else if (gameBoard[i][0] === gameBoard[i][2] && gameBoard[i][2] != null){
+				var blockPosition = box2A;
+				makeMark(blockPosition);
+				console.log("block position:["+i+"][2]")
+				//Check last two first
+			} else if (gameBoard[i][1] === gameBoard[i][2] && gameBoard[i][2] != null){
+				var blockPosition = box1A;
+				makeMark(blockPosition);
+				console.log("block position:["+i+"][2]")
+			}
+		}
+	}
+
+	var checkIfAlmostDiagonalWinner = function(){
+		for (var i = 0; i < 3; i++ ){
+			//Check first two first
+			if(gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] != null){
+				var blockPosition = box1C;
+				makeMark(blockPosition);
+				console.log("block position:["+i+"][2]")
+				//Check corners first
+			} else if (gameBoard[0][i] === gameBoard[2][i] && gameBoard[2][i] != null){
+				var blockPosition = box2A;
+				makeMark(blockPosition);
+				console.log("block position: [1]["+i+"]")
+				//Check last two first
+			} else if (gameBoard[1][i] === gameBoard[2][i] && gameBoard[2][i] != null){
+				var blockPosition = box1A;
+				makeMark(blockPosition);
+				console.log("block position: [0]["+i+"]")
+			}
+		}
+	}
+
+	// 	 else if(gameBoard[0][1] === gameBoard[1][1] && gameBoard[1][1] != null){
+	// 		verticalPts = 2;
+	// 		if(gameBoard[1][1] === gameBoard[2][1]){
+	// 			verticalPts = 3;
+	// 			console.log("winner in column 2")
+	// 		}
+	// 	} else if (gameBoard[0][2] === gameBoard[1][2] && gameBoard[1][2] != null){
+	// 		console.log("almost winner in column 3")
+	// 		verticalPts = 2;
+	// 		if (gameBoard[1][2] === gameBoard[2][2]){
+	// 			verticalPts = 3;
+	// 			console.log("winner in column 3")
+	// 		}
+	// 	}
+	// }
+
+
+	// Refactor checkIFWinnerIn3LetterArray(game[1])
+	// State who won!
+    // If Horizontal, given an array, check if xx_, x_x, or _xx
+    //     If array[0] === x && array[1]
+    //         winner approaching from left in row 1
+    //     if array[0] ===x && array[2]
+    //         winner approaching from corners in row 1
+    // If Vertical, given a nested array game, create a new array of column
+    //     tempColumn.push(array[0][0])
+    //     tempColumn.push(array[1][0])
+    //     tempColumn.push(array[2][0])
 
 
 
+
+
+	var checkIfWinner = function(){
+		checkIfVerticalWinner();
+		checkIfHorizontalWinner();
+		checkIfDiagonalWinner();
+		checkIfAlmostVerticalWinner();
+		checkIfAlmostHorizontalWinner();
+	}
+
+// Event listeners 
+	var addEventListeners = function(){
+		// Event listener for clicking on any box
+		box.on("click", function(){
+			makeMark(this);
+			checkLocation(this);
+			checkIfWinner();
+			switchPlayer();
+		})
+		// Event listener for "Play Computer" button
+		// Event listener for "Two-Player" game button
+	};	
+
+// Loads event listeners once page is loaded on browser
+	$(document).ready(function(){
+		addEventListeners();
+	});
 
 // });
